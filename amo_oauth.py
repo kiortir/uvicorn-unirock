7462367cffc9337686fdf20168192b1324bc8651
kiortir <84376533+kiortir@users.noreply.github.com>
2021-09-02 17:12:15 +0300

    version 0.4

--- amo_oauth.py
+++ amo_oauth.py
@@ -11,10 +11,10 @@ def get_new_tokens(token: str, token_type: str) -> dict:
     headers["Content-Type"] = "application/json"
 
     if token_type == 'refresh_token':
-        data = '{"%s":"04bfd9ed-3fc2-4eec-8f68-48adcb24960e","%s":"79ldWvYS2IfZEK3WdINDPPEfvfQ7BjVp1RAYHKYz1olfGtZGEDCeIG8UjCubWGod","grant_type":"refresh_token","refresh_token": "%s","redirect_uri":"https://unirock.ru/"}' % (
+        data = '{"client_id":"%s","client_secret":"%s","grant_type":"refresh_token","refresh_token": "%s","redirect_uri":"https://unirock.ru/"}' % (
             CLIENT_ID, CLIENT_SECRET, token)
     else:
-        data = '{"%s":"04bfd9ed-3fc2-4eec-8f68-48adcb24960e","%s":"79ldWvYS2IfZEK3WdINDPPEfvfQ7BjVp1RAYHKYz1olfGtZGEDCeIG8UjCubWGod","grant_type":"authorization_code","code": "%s","redirect_uri":"https://unirock.ru/"}' % (
+        data = '{"client_id":"%s","client_secret":"%s","grant_type":"authorization_code","code": "%s","redirect_uri":"https://unirock.ru/"}' % (
             CLIENT_ID, CLIENT_SECRET, token)
 
     response = requests.post(url, headers=headers, data=data, timeout=5)
